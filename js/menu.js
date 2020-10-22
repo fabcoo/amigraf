ad03e39ad12484947810d051c697cb614e0640b5
Fabien Coo <fabien.coo@amigraf.com>
2020-10-22 10:11:04 +0200

    Correction erreurs JS

--- js/menu.js
+++ js/menu.js
@@ -33,4 +33,4 @@ function openCity(evt, cityName) {
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+// document.getElementById("defaultOpen").click();
